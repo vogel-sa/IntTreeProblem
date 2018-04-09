ed23e9678e8c88615cdd93aee64f3d381289ded6
DecidingToast <svogel2bro@gmail.com>
2018-04-09 18:43:14 -0400

    Getting closer, removed bug in getLeftNeighbor and getRightNeighbor

--- IntTree/IntTree.py
+++ IntTree/IntTree.py
@@ -10,11 +10,11 @@ elif not argv[1].isdigit() and int(argv[0]) <= 0:
 
 # Return a list of the correct size for a binary tree of the given depth
 def buildBinTree(depth):
-    return [0] * (2**(depth))
+    return [0] * (2**(depth + 1))
 
 # Get the index of the first element at the given depth
 def getFirstIndexAtDepth(depth):
-    return int(2**(depth - 1)) - 1
+    return int(2**(depth)) - 1
 
 # Get the index of the last element at the given depth
 def getLastIndexAtDepth(depth):
@@ -24,18 +24,18 @@ def getLastIndexAtDepth(depth):
 def getParent(child):
     return int(floor((child - 1)/2))
 
-# Return index of left child, or -1 if it does not exist
+# Return index of left child
 def getLeftChild(parent):
     return 2*parent + 1
 
-# Return index of right child, or -1 if it does not exist
+# Return index of right child
 def getRightChild(parent):
     return 2*parent + 2
 
 # Return index of left neighbor, or -1 if it does not exist
 # Always returns -1 if node is not at depth
 def getLeftNeighbor(node, depth):
-    if getFirstIndexAtDepth(depth) >= node or getLastIndexAtDepth(depth) <= node:
+    if getFirstIndexAtDepth(depth) >= node or getLastIndexAtDepth(depth) < node:
         return -1
     else:
         return node - 1
@@ -43,7 +43,7 @@ def getLeftNeighbor(node, depth):
 # Return index of right neighbor, or -1 if it does not exist
 # Always returns -1 if node is not at depth
 def getRightNeighbor(node, depth):
-    if getFirstIndexAtDepth(depth) >= node or getLastIndexAtDepth(depth) <= node:
+    if getFirstIndexAtDepth(depth) > node or getLastIndexAtDepth(depth) <= node:
         return -1
     else:
         return node - 1
@@ -51,19 +51,21 @@ def getRightNeighbor(node, depth):
 depth = int(argv[1])
 tree = buildBinTree(depth)
 
-for d in range(1, depth + 1):
-    if (d == 1):
-        tree[0] = 1
-        print [1]
-    else:
-        first = getFirstIndexAtDepth(d)
-        last = getLastIndexAtDepth(d)
-        for element in range(first, last + 1):
-            value = tree[getParent(element)]
-            if element == getLeftChild(getParent(element)): # For the left child
-                parentLeftNeighbor = tree[getLeftNeighbor(getParent(element), d - 1)]
-                tree[element] = max(value, value + parentLeftNeighbor)
-            elif element == getRightChild(getParent(element)): # For the right child
-                parentRightNeighbor = tree[getRightNeighbor(getParent(element), d - 1)]
-                tree[element] = max(value, value + parentRightNeighbor)
-        print tree[first:last+1]
+for d in range(0, depth):
+    first = getFirstIndexAtDepth(d)
+    last = getLastIndexAtDepth(d)
+    for element in range(first, last + 1):
+        value = tree[getParent(element)]
+        if (d <= 0): # For the root
+            tree[element] = 1
+        elif element == getLeftChild(getParent(element)): # For the left child
+            parentLeftNeighbor = getLeftNeighbor(getParent(element), d-1)
+            if parentLeftNeighbor != -1:
+                value += tree[parentLeftNeighbor]
+            tree[element] = value
+        elif element == getRightChild(getParent(element)): # For the right child
+            parentRightNeighbor = getRightNeighbor(getParent(element), d-1)
+            if parentRightNeighbor != -1:
+                value += tree[parentRightNeighbor]
+            tree[element] = value
+    print tree[first:last+1]
